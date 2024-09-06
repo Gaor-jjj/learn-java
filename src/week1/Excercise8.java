2a35161ab8d88bc9b162a2de00fe6bb555669b1c
gaor-jjj <kaur.tamm@gmail.com>
2024-09-06 11:44:57 +0300

    Excercises 8,9,11

---
+++ src/week1/Excercise8.java
@@ -0,0 +1,13 @@
+package week1;
+import java.util.Scanner;
+
+public class Excercise8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type a number: ");
+        int num = Integer.parseInt(sc.nextLine());
+        System.out.println("Type another number: ");
+        int num2 = Integer.parseInt(sc.nextLine());
+        System.out.println("Sum of the numbers: " + (num + num2));
+    }
+}
