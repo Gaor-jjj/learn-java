465ffb6317ee1709c51340abd55209327d294301
gaor-jjj <kaur.tamm@gmail.com>
2024-09-10 09:55:52 +0300

    Excercises 26, 29, 30, 32

---
+++ src/week1/Excercise29.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class Excercise29 {
+    public static void main(String[] args) {
+        int num = 2;
+        while (num < 101) {
+            System.out.println(num);
+            num += 2;
+        }
+    }
+}
