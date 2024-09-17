7b071767dfc5ef7982ba04d5c769aa8c55cf0a19
gaor-jjj <kaur.tamm@gmail.com>
2024-09-17 09:11:01 +0300

    Excercises 39, 41

---
+++ src/week1/Excercise41.java
@@ -0,0 +1,33 @@
+package week1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Excercise41 {
+
+    private static int drawNumber() {
+        Random random = new Random();
+        return random.nextInt(100) + 1; // Generates a number between 1 and 100
+    }
+
+    public static void main(String[] args) {
+        int targetNumber = drawNumber();
+        Scanner scanner = new Scanner(System.in);
+        int guess = 0;
+        int guessCount = 0;
+
+        while (guess != targetNumber) {
+            System.out.print("Guess a number: ");
+            guess = scanner.nextInt();
+            guessCount++;
+
+            if (guess < targetNumber) {
+                System.out.println("The number is greater, guesses made: " + guessCount);
+            } else if (guess > targetNumber) {
+                System.out.println("The number is lesser, guesses made: " + guessCount);
+            } else {
+                System.out.println("Congratulations, your guess is correct!");
+            }
+        }
+    }
+}
