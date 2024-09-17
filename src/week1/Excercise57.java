87c92772597f10bab2e887ecbc8892423a9692a4
gaor-jjj <kaur.tamm@gmail.com>
2024-09-17 11:20:39 +0300

    Excercises 50, 57, 61

---
+++ src/week1/Excercise57.java
@@ -0,0 +1,26 @@
+package week1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Excercise57 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<>();
+
+        while(true) {
+            System.out.print("Enter a word: ");
+            String input = scanner.nextLine().trim();
+            if(input.isEmpty()) {
+                System.out.println("You typed the following words: ");
+                for (String word : words) {
+                    System.out.println(word);
+                }
+                break;
+            } else {
+                words.add(input);
+            }
+        }
+        scanner.close();
+    }
+}
